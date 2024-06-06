e6cd70a3da5b60e7adc8d8ad773887b27c6dcfd9
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-07 03:17:42 +0530

    mongo Connection and data storing

--- Flipkart-Scraping/Mobiles/Mobiles.py
+++ Flipkart-Scraping/Mobiles/Mobiles.py
@@ -1,8 +1,9 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import DbConnection as db
 
-brands = ['Redmi', 'Samsung', 'Vivo', 'Oppo', 'Poco', 'Motorola', 'Google', 'Apple', "Mi", 'Oneplus', 'Lava', 'Asus',
+brands = [ 'Google', 'Apple', "Mi", 'Oneplus', 'Lava', 'Asus',
           'Iqoo', 'Sony']
 
 for brand in brands:
@@ -20,10 +21,11 @@ for brand in brands:
     for link in page_links:
         response = requests.get(link)
         soup = BeautifulSoup(response.text, 'html.parser')
-        products = soup.find_all('div', attrs={'class': 'tUxRFH'})
+        products = soup.find_all('a', attrs={'class': 'CGtC98'})
+
         for product in products:
             img_element = product.find('img', attrs={'class': 'DByuf4'})
-            img = img_element.get('src') if img_element else None
+            cover_img = img_element.get('src') if img_element else None
 
             title_element = product.find('div', attrs={'class': 'KzDlHZ'})
             title = title_element.get_text(" ") if title_element else None
@@ -35,15 +37,57 @@ for brand in brands:
             price = price_element.get_text(" ") if price_element else None
 
             desc = product.find_all('li', attrs={'class': 'J+igdf'})
-            desc_list = []
+            desc_short = []
 
             for description in desc:
                 desc_ele = description.get_text(" ")
-                desc_list.append(desc_ele)
+                desc_short.append(desc_ele)
 
-            dict1 = {'title': title, 'rating': rating, 'price': price, 'desc': desc_list, 'img':img}
-            # print(dict1)
-            df = df._append(dict1, ignore_index=True)
+            link_element = product
+            link = product.attrs['href'] if link_element else None
+            c_link = "https://www.flipkart.com" + link
+            # print(c_link)
 
-    print(df)
-    df.to_csv(brand + ".csv")
+            response = requests.get(c_link)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            img_elements = soup.find_all('img', attrs={'class': '_0DkuPH'})
+            img_list = []
+            for img in img_elements:
+                img_list.append(img.attrs['src'])
+            desc_long = {}
+            desc_elements = soup.find_all('div', attrs={'class': 'pqHCzB'})
+
+            for desc_element in desc_elements:
+                desc_head_ele = desc_element.find('div', attrs={'class': '_9GQWrZ'})
+                desc_head = desc_head_ele.get_text(" ") if desc_head_ele else None
+                desc_p_ele = desc_element.find('p')
+                desc_p = desc_p_ele.get_text(" ") if desc_p_ele else None
+
+                dict1 = {str(desc_head): str(desc_p)}
+                desc_long.update(dict1)
+            # print(desc_full)
+
+            spec_containers = soup.find_all('div', attrs={'class': 'GNDEQ-'})
+            spec_full = {}
+            for container in spec_containers:
+                spec_head_ele = container.find('div', attrs={'class': '_4BJ2V+'})
+                spec_head = spec_head_ele.get_text(" ") if spec_head_ele else None
+                spec_rows = container.find_all('tr', attrs={'class': 'WJdYP6'})
+                spec_rows_dict = {}
+
+                for spec_row in spec_rows:
+                    spec_key_ele = spec_row.find('td', attrs={'class': '+fFi1w'})
+                    spec_key = spec_key_ele.get_text(" ") if spec_key_ele else None
+                    spec_value_ele = spec_row.find('li', attrs={'class': 'HPETK2'})
+                    spec_value = spec_value_ele.get_text(" ") if spec_value_ele else None
+                    dict1 = {str(spec_key): spec_value}
+                    spec_rows_dict.update(dict1)
+                spec_full.update({str(spec_head): spec_rows_dict})
+
+            response = db.add_to_mobiles(brand=brand, title=title, link=c_link, price=price, rating=rating,
+                                         desc_short=desc_short, cover_img=cover_img, img_list=img_list,
+                                         desc_long=desc_long, specification=spec_full)
+            print(response)
+
+            # df = df._append(dict1, ignore_index=True)
